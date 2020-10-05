378e934dd8d37e8580076ee7896796b22c57fe17
Andrew Lien <65378290+OTLandU@users.noreply.github.com>
2020-10-05 17:42:07 +0800

    Update Logout.js

--- frontend/src/Components/Logout/Logout.js
+++ frontend/src/Components/Logout/Logout.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Logout = ()=>{
-	return(
-		<div className='Logout-container'>
-			<p>Hello from logout.</p>
-		</div>
-	);
+export default class Logout extends Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.onClick = this.onClick.bind(this);
+    }
+    onClick(e)
+    {
+        e.preventDefault();
+        this.setState(
+            {
+                googleId: '0',
+                token: '0'
+            }
+        )
+        window.location = '/';
+    }
+
+    render()
+    {
+        return (
+            <div>
+                <h3> Logout </h3>
+                <form onClick={this.onClick}>
+                    <div>
+                        <input type="submit" value="Logout" className="btn btn-primary"/>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
-
-export default Logout;
