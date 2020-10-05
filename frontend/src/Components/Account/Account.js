2053be58916ddf912f115def72962a75f7234fc7
Anthony <anthonywongfl99@gmail.com>
2020-10-05 14:35:04 -0400

    Inventory page for admin

--- frontend/src/Components/Account/Account.js
+++ frontend/src/Components/Account/Account.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
 
 const Account = ()=>{
+
+	const getAccountInfo = ()=>{
+		return [
+			{
+				username: "Neil Tramsen",
+			},
+			{
+				email_address: "ntramsen112@gmail.com",
+			}
+		];
+	}
 	return(
 		<div className='account-container'>
 			<div className = 'account-header'>
-				<p>Your account details</p>
+				<h2>Your account details</h2>
 			</div>
 			<div className = 'account-content'>
-				<p>User name: NTramsen</p>
-				<p>Email address: ntramsen112@gmail.com</p>
-				<p>Student ID: 1234567</p>
-				<p>Password: ********</p>
-			</div>
+				<ul className='account-info-list'>
+					{getAccountInfo().map((info, index)=>{
+						return(
+							<li key={info} className="account-info-list-element">
+								<span>{info.username}</span>
+								<span>{info.email_address}</span>
+							</li>
+						)
+					})}
+				</ul>
+			</div>                
 			<div className = 'edit-account'>
 				<button type='button'>Edit details</button>
 				<button type='button'>Change password</button>
